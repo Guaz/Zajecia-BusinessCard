a5667330483970798a963c08412980998215ccfa
Guaz <marcinlwac@gmail.com>
2017-04-01 14:34:58 +0200

    first commit

---
+++ www/js/pgb.js
@@ -0,0 +1,33 @@
+function init() {
+	document.addEventListener("deviceready",onDeviceReady, false);
+}
+
+function onDeviceReady() {
+	navigator.notification.beep(1);
+}
+
+function deviceInfo() {
+
+    info = 'Hi, I am your smartphone :-)' + '\n' +
+			'=====' + '\n' +
+			'Device Name    : ' + device.name + '\n' +
+			'Device Cordova : ' + device.cordova + '\n' +
+			'Device Platform: ' + device.platform + '\n' +
+			'Device UUID    : ' + device.uuid + '\n' +
+			'Device Model   : ' + device.model + '\n' +
+			'Device Version : ' + device.version + '\n';
+
+    navigator.notification.alert(info);
+}
+
+function personalData() {
+
+    info = 
+            'Name : Marcin '   + '\n' + 
+            'Surname : Wac '   + '\n' + 
+            'Email : marcin.wac@onet.eu '  + '\n' +
+            'Field of Study: IT' + '\n';
+
+	    navigator.notification.alert(info);
+	
+}
